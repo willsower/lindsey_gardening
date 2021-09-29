f38f340c3ae45a52a65a1e037f0c2693cae96e02
willsower <jatr812@gmail.com>
2021-09-28 22:03:38 -0400

    Got home slideshow going and nav starting

--- js/main.js
+++ js/main.js
@@ -14,5 +14,5 @@ function showSlides() {
   
   slides[slideIndex-1].style.display = "block";  
 
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 2000); // Change image every 5 seconds
 }
